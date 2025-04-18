155bc64575d1877a557b0f326b39975bc2dc9867
yokoyama <tomo_natu888@yahoo.co.jp>
2025-04-19 03:29:24 +0900

    name修正

--- svelte.config.js
+++ svelte.config.js
@@ -20,7 +20,7 @@ const config = {
       strict: true
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/portfolio-site-2' : ''
+      base: process.env.NODE_ENV === 'production' ? '/portfolio-site' : ''
     }
   }
 };
