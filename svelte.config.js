3de15432337625f307878a8078f170ac82354463
yokoyama <tomo_natu888@yahoo.co.jp>
2025-04-19 14:35:06 +0900

    edit-yml

--- svelte.config.js
+++ svelte.config.js
@@ -11,8 +11,6 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
       pages: 'build',
       assets: 'build',
       fallback: 'index.html',
