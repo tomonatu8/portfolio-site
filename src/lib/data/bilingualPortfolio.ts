6db791997496051afa00a8fcdf248a7e3ccdd3ae
yokoyama <tomo_natu888@yahoo.co.jp>
2025-05-26 00:46:35 +0900

    upt-bio

--- src/lib/data/bilingualPortfolio.ts
+++ src/lib/data/bilingualPortfolio.ts
@@ -450,7 +450,7 @@ export const profile: LocalizedProfile = {
   image: '',
   // bioを配列からテキストに変更
   bio: bt(
-    'I am a third-year Ph.D. Candidate in Information Science and Technology specializing in optimization, mechanism design, algorithmic game theory (matching/allocation etc.), and machine learning. Recently, my research interests have focused particularly on the intersection of machine learning technologies, platforms, and economic fairness. Also, I have experience translating algorithms and AI systems into practical applications, with passion for bridging research with real-world implementation.',
+    'I am a third-year Ph.D. Candidate in Information Science and Technology specializing in optimization, mechanism design, algorithmic game theory (matching/allocation etc.), and machine learning. Recently, my interests have focused particularly on the intersection of machine learning technologies, platforms, and economic fairness. With practical experience in algorithm and AI implementation, I am passionate about bridging the gap between research and real-world applications.',
     '最適化，メカニズムデザイン，アルゴリズム的ゲーム理論（マッチング/配分など），機械学習分野を専門とする情報理工学系研究科の博士3年生です。最近は特にAI技術やプラットフォームと経済的な公平性の視点に興味があります。また，アルゴリズムや機械学習の実用化経験があり，研究と社会実装の架け橋に興味や情熱を持っています。'
   ),
   jobSeekingInfo: bt(
